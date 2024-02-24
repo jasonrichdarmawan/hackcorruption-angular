e781410f4d47eca5b96724184848a427d212bd4f
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2024-02-25 00:14:07 +0800

    build: npx ng add @angular/material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"hackcorruption-stg","appId":"1:120065507426:web:a82bf23d5209aa2f54113b","storageBucket":"hackcorruption-stg.appspot.com","apiKey":"AIzaSyD6qrf4e5g7RIfOvLiBF3EhjZQMl970lzc","authDomain":"hackcorruption-stg.firebaseapp.com","messagingSenderId":"120065507426"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"hackcorruption-stg","appId":"1:120065507426:web:a82bf23d5209aa2f54113b","storageBucket":"hackcorruption-stg.appspot.com","apiKey":"AIzaSyD6qrf4e5g7RIfOvLiBF3EhjZQMl970lzc","authDomain":"hackcorruption-stg.firebaseapp.com","messagingSenderId":"120065507426"}))), importProvidersFrom(provideFirestore(() => getFirestore())), provideAnimationsAsync()]
 };
