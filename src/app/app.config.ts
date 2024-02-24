956dc7c15976f4d9ccebfee65d5361b1d400649d
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2024-02-25 00:17:17 +0800

    chore: tidy up

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,13 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"hackcorruption-stg","appId":"1:120065507426:web:a82bf23d5209aa2f54113b","storageBucket":"hackcorruption-stg.appspot.com","apiKey":"AIzaSyD6qrf4e5g7RIfOvLiBF3EhjZQMl970lzc","authDomain":"hackcorruption-stg.firebaseapp.com","messagingSenderId":"120065507426"}))), importProvidersFrom(provideFirestore(() => getFirestore())), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebaseConfig))),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    provideAnimationsAsync()]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    "projectId": "hackcorruption-stg",
+    "appId": "1:120065507426:web:a82bf23d5209aa2f54113b",
+    "storageBucket": "hackcorruption-stg.appspot.com",
+    "apiKey": "AIzaSyD6qrf4e5g7RIfOvLiBF3EhjZQMl970lzc",
+    "authDomain": "hackcorruption-stg.firebaseapp.com",
+    "messagingSenderId": "120065507426",
+  },
+};
