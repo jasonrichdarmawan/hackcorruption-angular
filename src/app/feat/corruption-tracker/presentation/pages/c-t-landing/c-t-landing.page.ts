cf3bccb6f10d95668c86abce9037284e3ff7a177
mhanifrmd <46512664+mhanifrmd@users.noreply.github.com>
2024-02-25 13:19:07 +0800

    header (#6)
    Co-authored-by: Hanif <haniframadhan.mail@gmail.com>

--- src/app/feat/corruption-tracker/presentation/pages/c-t-landing/c-t-landing.page.ts
+++ src/app/feat/corruption-tracker/presentation/pages/c-t-landing/c-t-landing.page.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { CTFiltersComponent } from '../../components/c-t-filters/c-t-filters.component';
 import { CTSearchBarComponent } from '../../components/c-t-search-bar/c-t-search-bar.component';
 import { CTTableComponent } from '../../components/c-t-table/c-t-table.component';
+import { CTHeaderComponent } from '../../components/c-t-header/c-t-header.component';
+import { CTFooterComponent } from '../../components/c-t-footer/c-t-footer.component';
 
 @Component({
   selector: 'app-c-t-landing',
   standalone: true,
   imports: [
+    CTHeaderComponent,
+    CTFooterComponent,
     CTSearchBarComponent,
     CTFiltersComponent,
     CTTableComponent,
