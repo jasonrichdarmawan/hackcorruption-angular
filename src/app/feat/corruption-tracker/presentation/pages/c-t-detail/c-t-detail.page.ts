7d2df83d1ba0505bef6da1e83a61f12bca750a41
Naufal Fawwaz <fawaznaufal23@gmail.com>
2024-02-25 12:53:52 +0800

    finish detail page

--- src/app/feat/corruption-tracker/presentation/pages/c-t-detail/c-t-detail.page.ts
+++ src/app/feat/corruption-tracker/presentation/pages/c-t-detail/c-t-detail.page.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { GetCaseFirestoreDataSourceModel, GetCaseFirestoreDataSourceService } from '../../../../services/get-case-firestore-data-source-service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-c-t-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    AsyncPipe
+  ],
+  providers: [
+    GetCaseFirestoreDataSourceService
+  ],
   templateUrl: './c-t-detail.page.html',
   styleUrl: './c-t-detail.page.scss'
 })
-export class CTDetailPage {
+export class CTDetailPage implements OnInit {
+  id: string = "";
+  dataSource$: Observable<GetCaseFirestoreDataSourceModel>;
 
+  constructor(private route: ActivatedRoute, private service: GetCaseFirestoreDataSourceService) {
+    this.dataSource$ = new Observable();
+  }
+
+  private initializeGetCasesSubscription(activatedRoute: ActivatedRoute, getCases: GetCaseFirestoreDataSourceService) {
+    const subscription = activatedRoute.queryParams.subscribe(_ => {
+      this.dataSource$ = getCases.get({
+        id: this.id
+      })
+    })
+    return subscription;
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = params.get('id') ?? ""
+    })
+    this.initializeGetCasesSubscription(this.route, this.service);
+  }
+
+  toTitleCase(text: string): string {
+    return text.replace(
+      /\w\S*/g,
+      function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+    );
+  }
 }
